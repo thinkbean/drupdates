e22649ca885a9749f69e2bdb108d62d1d3d9d985
Mike Milano <coder1@gmail.com>
2017-02-08 09:15:17 -0800

    Removing debug line.

--- bin/drupdates.php
+++ bin/drupdates.php
@@ -6,8 +6,6 @@ require __DIR__.'/../vendor/autoload.php';
 use Drupdates\Command;
 use Symfony\Component\Console\Application;
 
-$foo = 'bar';
-
 $application = new Application('Drupdates', '0.1-dev');
 $application->add(new Command\DrupdatesCommand());
 $application->setDefaultCommand('drupal:updates', true);
